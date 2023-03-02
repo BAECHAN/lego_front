7183edfd0aa3e7037928458130e5e64ed92824d7
baechan <asdfjklddd@naver.com>
2023-03-02 23:10:29 +0900

    test

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -103,6 +103,7 @@ export default function Login() {
                 id="email"
                 placeholder="예) lego@lego.co.kr"
                 autoComplete="off"
+                value="asdfjklddd@gmail.com"
               />
             </label>
             <label>
@@ -113,6 +114,7 @@ export default function Login() {
                 name="password"
                 id="password"
                 autoComplete="off"
+                value="test1234!"
               />
               {isShowPw ? (
                 <FontAwesomeIcon
