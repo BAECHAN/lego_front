5095103ff60112dbd5a970b01e221655d1c17608
baechan <asdfjklddd@naver.com>
2023-04-18 19:25:19 +0900

    이미지 저사양 테스트

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -37,7 +37,7 @@ export default function ProductCard(props: { product: ProductT; key: number }) {
               placeholder="blur"
               blurDataURL={`iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFc
               SJAAAADUlEQVR42mN8sFeoHgAGZAIwFY0DHwAAAABJRU5ErkJggg==`}
-              quality={100}
+              quality={10}
               layout="responsive"
             />
           </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
-import { useEffect } from 'react'
+import { ObjT_Str } from 'types'
 import Layout from '../components/Layout'
 import Image from 'next/image'
 
-export default function Home() {
-  // useEffect(() => {
-  //   alert(
-  //     '해당 페이지는 포트폴리오 제출용 사이트로 상업적 목적을 띄지 않습니다.'
-  //   )
-  // }, [])
-
+export default function Home({ data }: { data: ObjT_Str }) {
   return (
     <div className="min-h-[602px] m-10">
       <div className="flex justify-center">
         <div className="home-image">
-          <Image
-            src="/lego_main.jpeg"
-            alt="레고 홈 화면 이미지"
-            layout="fill"
-          />
+          <Image src={data.imageUrl} alt="레고 홈 화면 이미지" layout="fill" />
         </div>
       </div>
       <div className="flex justify-center text-2xl mt-3">
@@ -46,4 +36,17 @@ export default function Home() {
 
 Home.getLayout = function getLayout(page: React.ReactElement) {
   return <Layout>{page}</Layout>
+}
+
+export async function getStaticProps() {
+  // 빌드 타임에 이미지 데이터를 불러옵니다.
+  const imageUrl = '/lego_main.jpeg'
+
+  return {
+    props: {
+      data: {
+        imageUrl,
+      },
+    },
+  }
 }
