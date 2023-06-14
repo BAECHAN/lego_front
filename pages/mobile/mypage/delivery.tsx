208cefc85ebeb61895cfe40720083d314bf867ce
baechan <asdfjklddd@naver.com>
2023-06-14 12:50:58 +0900

    [모바일] 마이페이지 작성중인 페이지 404 안뜨고 안내 문구 출력

--- pages/mobile/mypage/delivery.tsx
+++ pages/mobile/mypage/delivery.tsx
@@ -1,12 +1,12 @@
 import LayoutMobile from '@components/LayoutMobile'
 
-export default function UserInfo() {
+export default function Delivery() {
   return (
     <div className="flex justify-center h-10">
       <strong>작성 중인 페이지입니다.</strong>
     </div>
   )
 }
-UserInfo.getLayout = function getLayout(page: React.ReactElement) {
+Delivery.getLayout = function getLayout(page: React.ReactElement) {
   return <LayoutMobile>{page}</LayoutMobile>
 }
